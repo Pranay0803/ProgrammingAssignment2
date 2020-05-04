dc6907a96d9a9097525a02c7f923fe2d5f12d751
Pranay Yenagandula <60071693+Pranay0803@users.noreply.github.com>
2020-05-04 21:53:57 +0530

    Updating functions makeCacheMatrix and cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
+##This function will create the special matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv<-NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(i) inv <<- i
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+        inv<-x$getinv()
+        if(!is.null(inv)){
+                message("getting inverse")
+                return(inv)
+        }
+        data<-x$get()
+        inv=solve(data)
+        s$setinv(inv)
+        inv
 }
